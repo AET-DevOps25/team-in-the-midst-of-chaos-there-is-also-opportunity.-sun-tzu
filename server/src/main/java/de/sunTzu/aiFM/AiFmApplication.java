28517dc436f07125605c39a559f4b5b61837177b
kurtiii <kurt.stonus@mailbox.org>
2025-05-21 23:31:02 +0200

    Added Spring Boot server with hello world endpoint

---
+++ server/src/main/java/de/sunTzu/aiFM/AiFmApplication.java
@@ -0,0 +1,13 @@
+package de.sunTzu.aiFM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiFmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiFmApplication.class, args);
+	}
+
+}
---
+++ server/src/main/java/de/sunTzu/aiFM/HelloWorld.java
@@ -0,0 +1,12 @@
+package de.sunTzu.aiFM;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+    @GetMapping(value = "/greet", produces = "text/plain")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
