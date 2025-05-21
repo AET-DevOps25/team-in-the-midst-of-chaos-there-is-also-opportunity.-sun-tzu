9097f02b60314f0fe9ac8e8665916f2ed0701437
kurtiii <kurt.stonus@mailbox.org>
2025-05-21 23:31:02 +0200

    Rebase feature/audioStreaming onto main

---
+++ server/src/test/java/de/sunTzu/aiFM/AiFmApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sunTzu.aiFM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiFmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
