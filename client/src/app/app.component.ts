0e79bef2fd40c2afcbf395370953695ad76f3f89
pschlo <70105856+pschlo@users.noreply.github.com>
2025-05-24 17:10:13 +0200

    added play button component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { PlayButtonComponent } from "./play-button/play-button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, PlayButtonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
