659654b278f3aa69332fba8bff271b8bab8ccede
pschlo <70105856+pschlo@users.noreply.github.com>
2025-06-07 19:00:15 +0200

    changed project name; updated progress bar; updated backend to respond with JSON

--- server/PlaylistService/src/main/java/de/sunTzu/playlistApp/HelloWorld.java
+++ server/PlaylistService/src/main/java/de/sunTzu/playlistApp/HelloWorld.java
@@ -1,12 +1,15 @@
 package de.sunTzu.playlistApp;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorld {
     @GetMapping(value = "/greet", produces = "application/json")
-    public String sayHello() {
-        return "Hello World!";
+    public Map<String, String> sayHello() {
+        return Collections.singletonMap("message", "Hello World!");
     }
 }
