6b454f9b9d59ce63d99b3043eca4301d768aa0ab
Peter Schlosshan <70105856+pschlo@users.noreply.github.com>
2025-07-19 17:09:28 +0000

    updated setup script to include logging and version

--- setup.sh
+++ setup.sh
@@ -2,12 +2,18 @@
 
 set -euo pipefail
 
+
+echo "Starting ai.FM setup script"
+echo
+
 mkdir aifm
 cd aifm
+echo "Created application directory $PWD/aifm"
 
 curl -sS -o compose.yml https://raw.githubusercontent.com/AET-DevOps25/team-in-the-midst-of-chaos-there-is-also-opportunity.-sun-tzu/main/compose.yml
+echo "Downloaded $PWD/compose.yml"
 
-echo "Welcome to the ai.FM setup script!"
+
 echo
 echo -n "Please configure the application by entering values for the corresponding environment variables."
 echo -n " Sensitive values will not be shown in the terminal."
@@ -31,11 +37,20 @@ echo
 echo -n "URL [http://localhost:8080]: "
 IFS= read -r URL < /dev/tty
 
+echo -n "VERSION [latest]: "
+IFS= read -r VERSION < /dev/tty
+
+
 > .env
 echo "DOWNLOAD_PASS=$DOWNLOAD_PASS" >> .env
 echo "OPENAI_API_KEY=$OPENAI_API_KEY" >> .env
 echo "URL=$URL" >> .env
+echo "VERSION=$VERSION" >> .env
 
+echo "Wrote configuration to $PWD/.env"
+echo
+
+echo "Starting application"
 docker compose up -d --pull always
 
 docker compose logs -f
